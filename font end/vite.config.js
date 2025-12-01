7a4c17dc674e1e46f3cfb39e35d09f57448d9a7c
Eric <3505285017@qq.com>
2025-12-02 01:06:06 +0800

    Add files via upload

---
+++ font end/vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import cesium from 'vite-plugin-cesium'
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    cesium(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  // 确保服务监听所有 IP，方便局域网调试
+  server: {
+    host: '0.0.0.0'
+  }
+})
